d991e46ed99bcea2476674dc25ca43c5b81b6315
Gabriel-Almeida-Ajax <Gabriel-Almeida-Ajax@users.noreply.github.com>
2020-06-01 20:57:28 -0300

    chablau

--- URI/1019/index.js
+++ URI/1019/index.js
@@ -1,28 +1,12 @@
 var input = require('fs').readFileSync('./uri/1019/dev/stdin.txt', 'utf8');
-var screen = { hora: 00, min: 00, segun: 00 }
 
+var screen = adcHora(input)
 
-
-function adcSec(segundos) {
-    let seg = parseInt((segundos / 60 - adcMin(segundos)) * 100)
-    console.log('s')
-    return screen.segun = seg
+function adcHora(segundos, screen) {
+    let hora = parseInt(segundos / 60 / 60)
+    let min = parseInt((segundos - (hora * 60 * 60)) / 60)
+    let seg = segundos - (min * 60 + (hora * 60 * 60))
+    return { hora, min, seg };
 }
 
-function adcMin(segundos) {
-    let min = parseInt(segundos / 60)
-        //adcSec(segundos);
-    console.log('m')
-    return screen.min = min
-}
-
-function adcHora(segundos) {
-    let horas = parseInt((segundos / 60) / 60)
-    let min = adcMin(segundos)
-    let seg = adcSec(segundos)
-    return { horas, min, seg }
-}
-
-input > 3600 ? adcHora(input) : input > 60 ? adcMin() : adcSec(input);
-
-console.log(screen)
+console.log(`${screen.hora}:${screen.min}:${screen.seg}`)
