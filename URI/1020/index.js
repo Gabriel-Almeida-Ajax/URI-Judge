6fe75daa172b1a6b57d2dc5222c4e4aed7a27ef4
Gabriel-Almeida-Ajax <Gabriel-Almeida-Ajax@users.noreply.github.com>
2020-06-14 01:33:20 -0300

    Update index.js

--- URI/1020/index.js
+++ URI/1020/index.js
@@ -5,19 +5,26 @@ function global() {
 
     function conversao(input) {
         let public = {}
-        public.years = parseInt(input / 365)
-        public.months = parseInt((Math.round(((input / 365) - public.years) * 365)) / 30);
-        public.days = Math.round((Math.round(((input / 365) - public.years) * 365)) - public.months * 30);
 
+        let years = (input) => parseInt(input / 365);
+        let months = (public, input) => parseInt((Math.round(((input / 365) - public.years) * 365)) / 30);
+        let days = (public, input) => Math.round((Math.round(((input / 365) - public.years) * 365)) - public.months * 30);
+
+
+        public.years = years(input);
+        public.months = months(public, input)
+        public.days = days(public, input)
 
 
         return public;
     }
 
-    public.output = console.log(`${conversao(input).years} ano(s)\n${conversao(input).months} mes(es)\n${conversao(input).days} dia(s)`);
+    public.output = (`${conversao(input).years} ano(s)\n${conversao(input).months} mes(es)\n${conversao(input).days} dia(s)`);
 
     return public;
 
 }
 
-var public = global()
+var public = global
+
+console.log(public().output)
